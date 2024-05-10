5b2e1e414e172d4bb4202a553de52f072e36b773
Facundo Abdo <facundo.abdo@globant.com>
2024-05-10 14:57:18 -0300

    delete spec files

--- src/app/dialog/dialog-ref.ts
+++ src/app/dialog/dialog-ref.ts
@@ -3,19 +3,10 @@ import { Observable, Subject } from "rxjs";
 import { ComponentRef } from "@angular/core";
 
 export class DialogRef<R = unknown, C = unknown> {
-  /**
-   * Instance of component opened into the dialog. Will be
-   * null when the dialog is opened using a `TemplateRef`.
-   */
   readonly componentInstance: C | undefined;
 
-  /**
-   * `ComponentRef` of the component opened into the dialog. Will be
-   * null when the dialog is opened using a `TemplateRef`.
-   */
   readonly componentRef: ComponentRef<C> | undefined;
 
-  /** Emits when the dialog has been closed. */
   readonly closed: Observable<R | undefined> = new Subject<R | undefined>();
 
   close(result?: R): void {
